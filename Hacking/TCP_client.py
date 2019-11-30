332a4506b3d6d765ddc2852260efea0c132f8734
Josh <jocoso@users.noreply.github.com>
2019-11-30 18:31:26 +0000

    Add files via upload

---
+++ Hacking/TCP_client.py
@@ -0,0 +1,26 @@
+import socket
+
+#XXX: Assumptions made
+#        -Our connection will always succeed
+#        -The server expect for us to send data first
+#        -The server will send us a response in a timely fashion
+
+target_host = "www.google.com"
+target_port = 80
+
+#Create a socket object
+#AF_INET: We are going to be using standard ipv4 address or hostnames
+#SOCK_STREAM: Describes it will be a TCP client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Make connection with the client
+client.connect((target_host, target_port))
+
+#Send some data 
+client.send("GET / HTTP/1.1\NHost: google.com\r\n\r\n")
+
+#Receive some data
+response = client.recv(4096)
+
+print(response)
+
